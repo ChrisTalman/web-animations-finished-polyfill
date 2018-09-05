7aba5c23f7f002157a576035c4030f0c92af0e2a
Chris Talman <cewe.wow@gmail.com>
2018-09-05 14:38:49 +0100

    Only polyfill once the document exists; moved code into immediately invoked function expression.

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 // Courtesy of Šime Vidas: https://gist.github.com/simevidas/2e721c8e6d67f04b5e1a0083c542a767
 
-// only polyfill .finished in browsers that already support animate()
-if (document.body.animate)
-{
-	// Chrome does not seem to expose the Animation constructor globally
-	if (typeof Animation === 'undefined')
+(
+	function()
 	{
-		window.Animation = document.body.animate({}).constructor;
-	};
-	if (Animation.prototype.finished === undefined)
-	{
-		Object.defineProperty
-		(
-			Animation.prototype,
-			'finished',
+		function polyfill()
+		{
+			// only polyfill .finished in browsers that already support animate()
+			if (document.body.animate)
 			{
-				get()
+				// Chrome does not seem to expose the Animation constructor globally
+				if (typeof Animation === 'undefined')
 				{
-					if (!this._finished)
-					{
-						this._finished = this.playState === 'finished' ?
-							Promise.resolve() :
-							new Promise
-							(
-								(resolve, reject) =>
+					window.Animation = document.body.animate({}).constructor;
+				};
+				if (Animation.prototype.finished === undefined)
+				{
+					Object.defineProperty
+					(
+						Animation.prototype,
+						'finished',
+						{
+							get()
+							{
+								if (!this._finished)
 								{
-									this.addEventListener('finish', resolve, {once: true});
-									this.addEventListener('cancel', reject, {once: true});
-								}
-							);
-        			};
-        			return this._finished;
-      			}
-    		}
-		);
-	};
-};
+									this._finished = this.playState === 'finished' ?
+										Promise.resolve() :
+										new Promise
+										(
+											(resolve, reject) =>
+											{
+												this.addEventListener('finish', resolve, {once: true});
+												this.addEventListener('cancel', reject, {once: true});
+											}
+										);
+			        			};
+			        			return this._finished;
+			      			}
+			    		}
+					);
+				};
+			};
+		};
+		if (document.readyState === 'interactive')
+		{
+			polyfill();
+		}
+		else
+		{
+			document.addEventListener('DOMContentLoaded', polyfill);
+		};
+	}
+)();
